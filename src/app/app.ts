d465b2d90e6a983049e8e80a9a1dfcf5177a1495
Thrinesh <thrineshyadav476@gmail.com>
2026-01-19 09:41:08 +0530

    Fix GitHub Pages index path

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./budget-planer/login/login";
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet,CommonModule,MatIconModule],
+  imports: [RouterOutlet, CommonModule, MatIconModule, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
